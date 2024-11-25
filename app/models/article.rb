c149b945674334aec523647e42eb007de7050d19
Abror Boqiyev <Boqiyevabror@gmail.com>
2024-11-25 17:10:27 +0500

    added status in model/article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,14 @@
 class Article < ApplicationRecord
+  has_many :comments
+
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 
-  has_many :comments
+  VALID_STATUES = [ "public", "private", "archived" ]
+
+  validates :status inclusions: { in: VALID_STATUES }
+
+  def archived
+    status == "archived"
+  end
 end
